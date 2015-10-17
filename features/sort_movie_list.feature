2bcd451a86d727ae561ca6869c59a7d4821e30ac
hhameed <haider-hameed@uiowa.edu>
2015-10-17 01:12:34 +0000

    looks like i am done

--- features/sort_movie_list.feature
+++ features/sort_movie_list.feature
@@ -22,8 +22,10 @@ Background: movies have been added to database
   And I am on the RottenPotatoes home page
 
 Scenario: sort movies alphabetically
-  # your scenario here
+  When I have opted to sort movies by: "Title"
+  Then I should see "Aladdin" before "Amelie"
 
 Scenario: sort movies in increasing order of release date
-  # your scenario here
+  When I have opted to sort movies by: "Release Date"
+  Then I should see "Raiders of the Lost Ark" before "Chicken Run"
 
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -46,29 +46,52 @@ Given /^I am on the RottenPotatoes home page$/ do
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
-  pending  # Remove this statement when you finish implementing the test step
   movies_table.hashes.each do |movie|
-    # Each returned movie will be a hash representing one row of the movies_table
-    # The keys will be the table headers and the values will be the row contents.
-    # Entries can be directly to the database with ActiveRecord methods
-    # Add the necessary Active Record call(s) to populate the database.
+    Movie.create(movie);
   end
 end
 
 When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
-  # HINT: use String#split to split up the rating_list, then
-  # iterate over the ratings and check/uncheck the ratings
-  # using the appropriate Capybara command(s)
-  pending  #remove this statement after implementing the test step
+  uncheck('ratings_G')
+  uncheck('ratings_PG')
+  uncheck('ratings_PG-13')
+  uncheck('ratings_R')
+  uncheck('ratings_NC-17')
+  arg1.split(', ').each do |rating|
+    check('ratings_'+rating)
+  end
+  click_button('ratings_submit')
 end
 
 Then /^I should see only movies rated: "(.*?)"$/ do |arg1|
-  pending  #remove this statement after implementing the test step
+  ratings = arg1.split(', ')
+  result=true
+  all("tbody tr").each do |tr|
+    txt = tr.all('td')[1].text
+    result &&= ratings.include?(txt)
+    puts txt + "\n"
+  end
+  result &&= Movie.where(:rating=>ratings).count() == all("tbody tr").count
+  expect(result).to be_truthy
 end
 
 Then /^I should see all of the movies$/ do
-  pending  #remove this statement after implementing the test step
+  result = all("tbody tr").count == 10
+  expect(result).to be_truthy
 end
 
 
+When /^I have opted to sort movies by: "(.*?)"$/ do |arg1|
+  sort_by = arg1.downcase.gsub(' ', '_') + '_header'
+  click_link(sort_by)
+  all("tbody tr").each do |tr|
+    txt = tr.all('td')[0].text
+    result &&= ratings.include?(txt)
+    puts txt + "\n"
+  end
+end
 
+Then /^I should see "(.*?)" before "(.*?)"$/ do |movie1,movie2|
+  result = /#{movie1}.*#{movie2}/m =~ page.body
+  expect(result).to be_truthy
+end
